1210c4747d05bc2ad9ddb8c10124f94200f08323
Nat <ntaktach@gmail.com>
2021-01-22 12:37:08 -0500

    commit tree d619825d31771e846609929e47e13139b3788fd0

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,10 @@
-# Add code from Readme
+class CreateBirds < Activerecord::[Migration[5.2]
+    def change 
+        create table :birds do |t| 
+            t.string :name 
+            t.string :species 
+            t.timestamps
+        end
+    end
+end
+
